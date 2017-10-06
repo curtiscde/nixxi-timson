ad34043bcb0b6624846258d72daf26cce82a0086
Curtis Timson <curt@live.co.uk>
2017-10-06 20:29:55 +0100

    Update test

--- static/nx/flickr.test.js
+++ static/nx/flickr.test.js
@@ -11,13 +11,17 @@ describe('service: flickrApi', function(){
   it('returns correct flickr Api Url', function(){
 
     var apiKey = '264fbe68492fe1c3e4e1696f67112bfa';
-    var flickrId = '36940360742';
+    var flickrId = '72157686321315123';
     var userId = '152071786@N03';
 
     var actualUrl = _flickrApiService.getFlickrApiUrl(apiKey, flickrId, userId);
-    var expectedUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=264fbe68492fe1c3e4e1696f67112bfa&photoset_id=36940360742&user_id=152071786@N03&extras=url_m%2C+url_o&format=json&nojsoncallback=1';
+    var expectedUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=264fbe68492fe1c3e4e1696f67112bfa&photoset_id=72157686321315123&user_id=152071786@N03&extras=url_m%2C+url_o&format=json&nojsoncallback=1';
 
     expect(expectedUrl).toBe(actualUrl);
+
+  });
+
+  it('returns flickr album object', function(){
 
   });
 
